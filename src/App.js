6b970f25e28070294b6922e33679707864a7afe6
Júlio Xavier <julioxavierr@gmail.com>
2022-03-11 19:19:14 -0300

    init react project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import "./App.css";
+
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">Connect your Ethereum wallet and wave at me!</div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
