78d7919f22a5fd39cb8c2a39857baea1052fb6c9
Júlio Xavier <julioxavierr@gmail.com>
2022-03-12 13:56:41 -0300

    handle connecting to ethereum account

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import "./App.css";
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  useEffect(() => {
+    const checkIfWalletIsConnected = async () => {
+      try {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+        } else {
+          console.log("We have the ethereum object", ethereum);
+        }
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    checkIfWalletIsConnected();
+  }, []);
+
+  const handleConnectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const wave = () => {};
 
   return (
@@ -12,9 +66,15 @@ export default function App() {
 
         <div className="bio">Connect your Ethereum wallet and wave at me!</div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
+        {currentAccount ? (
+          <button className="waveButton" onClick={wave}>
+            Wave at Me
+          </button>
+        ) : (
+          <button className="waveButton" onClick={handleConnectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
