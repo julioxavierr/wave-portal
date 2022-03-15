3155b2cb71e4ed88fdb0a11e87a11cb8eb191732
Júlio Xavier <julioxavierr@gmail.com>
2022-03-14 21:52:09 -0300

    fund the contract with eth

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,9 @@ const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
   // create a local ethereum network
-  const waveContract = await waveContractFactory.deploy();
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
 
   // wait until the contract is deployed
   await waveContract.deployed();
@@ -13,16 +15,30 @@ const main = async () => {
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  // Get contract balance
+  const initialContractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(initialContractBalance)
+  );
+
   const initialWaveCount = await waveContract.getTotalWaves();
   console.log(initialWaveCount.toNumber());
 
-  const firstWaveTxn = await waveContract.wave("A message!");
-  await firstWaveTxn.wait(); // Wait for the transaction to be mined
+  // Send wave
+  const waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  const secondWaveTxn = await waveContract
-    .connect(randomPerson)
-    .wave("Another message!");
-  await secondWaveTxn.wait(); // Wait for the transaction to be mined
+  // Get Contract balance to see what happened!
+  const contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
   const allWaves = await waveContract.getAllWaves();
   console.log(allWaves);
